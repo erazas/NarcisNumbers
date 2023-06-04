a361bd7fb854d43d74372f36ed58c3ff7a8a8dca
Student <Student@VTC30-PC>
2023-06-04 19:28:37 +0400

    Change

--- Program.cs
+++ Program.cs
@@ -10,15 +10,16 @@
         static void NarcisNum()
         {
             Console.WriteLine("Please Enter the number to check whether the number Narcistic or not");
-            string Number = Console.ReadLine();                 //  Getting number from the user.
-            string CvToStr;                                     //  Made because the Convert.ToInt32 Method doesn't work correct for chars. 
-            int Sum = 0;                                        
-            bool CheckNarcis;                                   //  To print the final result for the user.
+            string Number = Console.ReadLine();                         //  Getting number from the user.
+            string CvToStr;                                             //  Made because the Convert.ToInt32 Method doesn't work correct for chars. 
+            int Sum = 0;
+            int digitcount = DigitCount(Convert.ToInt32(Number));
+            bool CheckNarcis;                                           //  To print the final result for the user.
             for (int i = 0; i < Number.Length; i++)             
             {
-                CvToStr = Number[i] + "0";                      //  the string for each letter.
+                CvToStr = Number[i] + "0";                              //  the string for each letter.
                 int EachNum = Convert.ToInt32(CvToStr) / 10;
-                Sum = Sum + Pow3(EachNum);
+                Sum = Sum + PowDigitCount(EachNum,digitcount);
             }
 
             if (Sum == Convert.ToInt32(Number))
@@ -32,9 +33,24 @@
 
             Console.WriteLine(CheckNarcis);
         }
-        static int Pow3(int number)                             //  The Method to get the ^3 of number
+        static int DigitCount(int number)                               // Method which gets the count of digits of the number
         {
-            return number * number * number;
+            int count = 0;
+            while (number > 0) 
+            {
+                number = number / 10;
+                count++;
+            }
+            return count;
+        }
+        static int PowDigitCount(int number, int count)          //  The Method to get the ^digitcount of number
+        {
+            int StartPow = 1;
+            for (int i = 1; i<= count; i++)
+            {
+                StartPow = StartPow * number;
+            }
+            return StartPow;
         }
     }
 }
